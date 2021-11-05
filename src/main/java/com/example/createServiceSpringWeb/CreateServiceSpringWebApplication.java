b19586da69525a37d5a4f42fe57afc1fdf92a4f0
Ivan <menkov_99@mail.ru>
2021-11-06 01:14:42 +0300

    create

---
+++ src/main/java/com/example/createServiceSpringWeb/CreateServiceSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.createServiceSpringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateServiceSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateServiceSpringWebApplication.class, args);
+	}
+
+}
